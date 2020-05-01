0790062d6202e05a63b4cb194c0d3e6dd2eba586
Sefat Anam <sefatanam@gmail.com>
2020-05-01 15:57:13 +0600

    integration successed

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import { TextField } from "react-native-material-textfield";
-import { View, SafeAreaView } from "react-native";
+import {
+  TextField,
+  OutlinedTextField,
+  FilledTextField,
+} from "react-native-material-textfield";
+import { View, Text, StyleSheet } from "react-native";
 
 export default class App extends Component {
   state = {
@@ -11,15 +15,49 @@ export default class App extends Component {
     let { phone } = this.state;
 
     return (
-      <SafeAreaView>
-        
+      <View style={styles.container}>
         <TextField
+        
           label="Phone number"
           value={phone}
           onChangeText={(phone) => this.setState({ phone })}
         />
-        
-      </SafeAreaView>
+
+        <OutlinedTextField
+        style={styles.color}
+          label="Phone number"
+          value={phone}
+          onChangeText={(phone) => this.setState({ phone })}
+        />
+        <FilledTextField
+          style={styles.color}
+          label="Phone number"
+          value={phone}
+          onChangeText={(phone) => this.setState({ phone })}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+container:{
+  width:350,
+  padding:20,
+  margin:8,
+  justifyContent:"center",
+  flex:1,
+  alignContent:'space-around'
+  
+  
+}
+,
+color:{
+  color:"#6200ee",
+  marginBottom:10,
+  flexDirection:'row',
+  flex:1,
+  justifyContent:'center'
+  
+}
+});
